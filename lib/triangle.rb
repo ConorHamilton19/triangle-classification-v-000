e499a84d69d11337345ee3e87e4af91c8bd402c2
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 07:37:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,13 @@ class Triangle
   def kind 
     if @length_1 == @length_2 && @length_2 == @length_3
       :equilateral
+    elsif
+      (@length_1 == @length_2 && @length_2 != @length_3) ||
+      (@length_1 == @length_3 && @length_2 != @length_1) ||
+      (@length_2 == @length_3 && @length_2 != @length_1)
+      :isosceles
     end 
+    
   end 
   
 end
