d0b588aeb1703895afc6fc4cf30ee0ae83e6fd55
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 07:38:30 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,9 @@ class Triangle
       (@length_1 == @length_3 && @length_2 != @length_1) ||
       (@length_2 == @length_3 && @length_2 != @length_1)
       :isosceles
+    elsif
+      @length_1 != @length_2 && @length_2 != @length_3
+      :scalene
     end 
     
   end 
