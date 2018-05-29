e03ac386899bf4943be215111694de9d2dffb121
Conor Hamilton <conhamil19@gmail.com>
2018-05-29 07:48:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,13 +17,14 @@ class Triangle
     elsif
       @length_1 != @length_2 && @length_2 != @length_3
       :scalene
-    else
+    elsif 
+     @length_1 == 0 || @length_2 == 0 || @length_3 == 0
       raise TriangleError 
     end 
   end 
   
   class TriangleError < StandardError
-    if @length_1 == 0 || @length_2 == 0 || @length_3 == 0
+    
     end
   end
   
